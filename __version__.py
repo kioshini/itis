24c78c864cb0da2815985ebc8a3c0f2423d19164
kioshini <kioshini@users.noreply.github.com>
2025-11-30 17:00:31 +0500

    Add version info

---
+++ __version__.py
@@ -0,0 +1,8 @@
+"""
+Информация о версии проекта
+"""
+
+__version__ = "1.0.0"
+__author__ = "kioshini"
+__description__ = "Telegram bot for intelligent game search with AI integration"
+__repository__ = "https://github.com/kioshini/itis"
